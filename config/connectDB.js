b883c4278de8e8a563161b7e47bffcd4851e4019
tarkorn1205 <name_1205@hotmail.com>
2022-03-03 18:53:33 +0700

    update

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql");
 
 let connection = mysql.createConnection({
-  host: "itdev.cmtc.ac.th",
-  port: "43306",
-  user: "tarkorn@2022",
-  password: "tarkorn@2022",
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "",
   database: "booking_tech",
   multipleStatements: true,
 });
 connection.connect();
 
 module.exports = connection;
+// host: "itdev.cmtc.ac.th",
+//   port: "43306",
+//   user: "tarkorn@2022",
+//   password: "tarkorn@2022",
+//   database: "booking_tech",
